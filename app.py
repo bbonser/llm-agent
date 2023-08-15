ea65a0649ad81223c4f32574c39b9161bf4d3599
Brett Bonser <bbonser89@gmail.com>
2023-08-15 19:30:22 -0400

    update

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ for idx, msg in enumerate(msgs.messages):
                 st.write(f"**{step[1]}**")
         st.write(msg.content)
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input(placeholder="What is the tallest mountain in the world?"):
     st.chat_message("user").write(prompt)
 
     if not openai_api_key:
